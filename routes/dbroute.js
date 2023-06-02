80d816051cd4f7829a8f27f152afb54eeb95eb42
Darren Jo <darren12@rocketmail.com>
2023-06-02 21:38:57 +0700

    Add reward routes

--- routes/dbroute.js
+++ routes/dbroute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { db } = require('../models/dbconnection');
 
-router.get('/readdata', (req, res) => {
+router.get('/sampah', (req, res) => {
     const sqlQuery = "SELECT * FROM sampah";
 
     db.query(sqlQuery, (err, result) => {
@@ -12,5 +12,18 @@ router.get('/readdata', (req, res) => {
         }
     });
 });
+
+router.get('/rewards', (req, res) => {
+    const sqlQuery = "SELECT * FROM rewards";
+
+    db.query(sqlQuery, (err, result) => {
+        if(err){
+            res.send(err).status(400);
+        }else{
+            res.send(result).status(200);
+        }
+    });
+});
+
 
 module.exports = router;
